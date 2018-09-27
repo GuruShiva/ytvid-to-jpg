e104f8eb18bf06db0492a9410d5d21b4d23e492b
Mir <21161840+TiredMir@users.noreply.github.com>
2018-09-28 01:12:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import os
 import json
 import youtube_dl
 import cv2
+import numpy as np
 
 
 def signal_handler(sig, frame):
@@ -26,42 +27,42 @@ def downloader():
     global file_title
     ydl_opts = {'forcefilename': True, 'forcetitle': True, 'outtmpl': '%(title)s', 'forcejson': True,}
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        search_title = str(input("Enter title:\n"))
-        amount = str(input(
-        "How many videos would you like to be downloaded?\n"))
-        result = ydl.extract_info("ytsearch{}:{}".format(amount, search_title))
-        file_title = ydl._filename
-        print(file_title)
-
-        
-def splitter():
-    cap = cv2.VideoCapture(file_title)
-    try:
-        if not os.path.exists('data'):
-            os.makedirs('data')
-    except OSError:
-        print ('There was an error creating the data directory')
-    currentFrame = 0
-    while(True):
-    # Capture frame-by-frame
-        ret, frame = cap.read()
-    # Saves the current frame in jpg file
-    name = './data/frame' + str(currentFrame) + '.jpg'
-    print ('Creating...' + name)
-    cv2.imwrite(name, frame)
-    # To stop duplicate images
-    currentFrame += 1
-# When everything is done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+        # search_title = str(input("Enter title:\n"))
+        # amount = str(input(
+        # "How many videos would you like to be downloaded?\n"))
+        search_title = str(input("Enter a video URL:\n"))
+        # result = ydl.extract_info("ytsearch{}:{}".format(amount, search_title))
+        result = ydl.extract_info(search_title)
+        print('----------------------------')
+        print(result['title'])
+        print('----------------------------')
+# def splitter():
+#     cap = cv2.VideoCapture(file_title)
+#     try:
+#         if not os.path.exists('data'):
+#             os.makedirs('data')
+#     except OSError:
+#         print ('There was an error creating the data directory')
+#     currentFrame = 0
+#     while(True):
+#     # Capture frame-by-frame
+#         ret, frame = cap.read(
+#     # Saves the current frame in jpg file
+#     name = './data/frame' + str(currentFrame) + '.jpg'
+#     print ('Creating...' + name)
+#     cv2.imwrite(name, frame)
+#     # To stop duplicate images
+#     currentFrame += 1
+# # When everything is done, release the capture
+#     cap.release()
+#     cv2.destroyAllWindows()
 
 
 
 def main():
     signal.signal(signal.SIGINT, signal_handler)
     downloader()
-    splitter()
+    # splitter()
 
-    
 if __name__ == "__main__":
     main()
